4c5b75da5399a5afee1e49f56bb2d2901a6d67b4
zhang <iyunx@qq.com>
2022-10-17 21:27:46 +0800

    index page item

---
+++ src/components/Item/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="flex text-sm" :class="{'border-b pb-3 mb-3': showBorder }">
+    <RouterLink to=""><img class="w-12 h-12 rounded" src="https://www.zcfsjt.com/storage/images/2022/0910/1_20220910095308_ulX2ho6tol.jpg" alt=""></RouterLink>
+    <div class="ml-3 flex-1">
+      <RouterLink to="" class="text-white bg-blue-600 mr-2 px-1.5 py-0.5 rounded">提问</RouterLink>
+      <RouterLink to="">海上生明月 天涯共此时</RouterLink>
+      <p class="mt-2.5 text-gray-600 space-x-3"><span>Admin</span> <span>2022-10-17</span> <span class="iconfont ">20</span></p>
+    </div>
+    <aside class="text-center relative">
+      <span class="text-white bg-blue-600 mr-2 px-1.5 py-0.5 rounded">精华</span>
+      <span class="text-white bg-red-600 mr-2 px-1.5 py-0.5 rounded">热门</span>
+      <p class=" absolute left-0 bottom-0">123</p>
+    </aside>
+  </li>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    showBorder: boolean
+  }>(), {
+    showBorder: false
+  }
+)
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,57 @@
 <template>
   <div class="flex">
-    <section class="flex-1">123</section>
+    <section class="flex-1">
+      <section class="bg-white border">
+        <div class="flex px-3 border-b py-2">
+          <span class="flex flex-1 space-x-3">
+            <RouterLink to="">综合</RouterLink>
+            <RouterLink to="">综合</RouterLink>
+            <RouterLink to="">综合</RouterLink>
+            <RouterLink to="">综合</RouterLink>
+          </span>
+          <span class="flex space-x-3">
+            <RouterLink to="">按最新</RouterLink>
+            <RouterLink to="">按热议</RouterLink>
+          </span>
+        </div>
+        <ul class="p-3">
+          <LiItem v-for="i in 10" :key="i" :showBorder="i !== 10" />
+        </ul>
+      </section>
+      <section class="bg-white border mt-5">
+        <div class="flex px-3 border-b py-2">
+          <span class="flex flex-1 space-x-3">
+            <RouterLink to="">综合</RouterLink>
+            <RouterLink to="">综合</RouterLink>
+            <RouterLink to="">综合</RouterLink>
+            <RouterLink to="">综合</RouterLink>
+          </span>
+          <span class="flex space-x-3">
+            <RouterLink to="">按最新</RouterLink>
+            <RouterLink to="">按热议</RouterLink>
+          </span>
+        </div>
+        <ul class="p-3">
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+          <li>123</li>
+        </ul>
+      </section>
+    </section>
     <section class="w-80 ml-5">
       <div class="bg-white text-sm pb-3">
+        <h3 class="px-3 py-2 border-b border-dashed">温馨通道</h3>
+        <ul class="grid grid-cols-2 mx-3 text-center border border-b-0 border-r-0 mt-3">
+          <li class="border-r border-b py-2">gitlab</li>
+          <li class="border-r border-b py-2">gitlab</li>
+          <li class="border-r border-b py-2">gitlab</li>
+          <li class="border-r border-b py-2">gitlab</li>
+          <li class="border-r border-b py-2">gitlab</li>
+          <li class="border-r border-b py-2">gitlab</li>
+        </ul>
+      </div>
+      <div class="bg-white text-sm pb-3 mt-5">
         <h3 class="px-3 py-2 border-b border-dashed">温馨通道</h3>
         <ul class="grid grid-cols-2 mx-3 text-center border border-b-0 border-r-0 mt-3">
           <li class="border-r border-b py-2">gitlab</li>
@@ -17,3 +66,6 @@
   </div>
 </template>
 
+<script setup lang="ts">
+import LiItem from '@/components/Item/index.vue'
+</script>
